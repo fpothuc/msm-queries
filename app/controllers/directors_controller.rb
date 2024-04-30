d6e411db30d01cf0602785adac1507825a4426c8
fpothuc <164956630+fpothuc@users.noreply.github.com>
2024-04-30 18:02:44 +0000

    each director info working

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,12 @@ class DirectorsController < ApplicationController
   def index
     render({ :template => "director_templates/list"})
   end
+
+  def show
+    dir_id = params.fetch("dir_id")
+    matching_records = Director.where({ :id => dir_id })
+    @the_director = matching_records.at(0)
+
+    render({ :template => "director_templates/details"})
+  end
 end
