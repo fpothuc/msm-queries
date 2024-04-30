afcada019eec92c6f4313953fa3c3e635b89e0de
fpothuc <164956630+fpothuc@users.noreply.github.com>
2024-04-30 18:20:10 +0000

    filled out routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get("directors", { :controller => "directors", :action => "index"})
   get("directors/:dir_id", {:controller => "directors", :action => "show"})
 
-  get("actors", { :controller => "", :action => ""})
+  get("actors", { :controller => "actors", :action => "index"})
+  get("actors/:actor_id", { :controller => "actors", :action => "show"})
 
-  get("movies", { :controller => "", :action => ""})
+  get("movies", { :controller => "movies", :action => "index"})
+  get("movies/:movie_id", { :controller => "movies", :action => "show"})
 end
