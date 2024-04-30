2a7f2bca51ab09e9719c1f1d9bc86e26b546fe2b
fpothuc <164956630+fpothuc@users.noreply.github.com>
2024-04-30 17:17:52 +0000

    added routes and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("directors", {})
+
+  get("actors", { :controller})
+
+  get("movies", {})
 end
