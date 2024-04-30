465d697f900dccfc5c45b43902414807ddceda6d
fpothuc <164956630+fpothuc@users.noreply.github.com>
2024-04-30 18:24:31 +0000

    modified movie and actors controllers

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,13 @@
 class ActorsController < ApplicationController
-  def #
-    render({ :template => "actor_templates/"})
+  def index
+    render({ :template => "actor_templates/list"})
+  end
+
+  def show
+    actor_id = params.fetch("actor_id")
+    matching_records = Movie.where({ :id => actor_id })
+    @the_actor = matching_records.at(0)
+
+    render({ :template => "actor_templates/details"})
   end
 end
